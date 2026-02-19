31abfd66f325995456271609a3699f38ba7bf18f
faroutroy <68562263+faroutroy@users.noreply.github.com>
2026-02-19 19:27:25 +0000

    Create SpSmartSearch.module.scss.ts

---
+++ src/webparts/spSmartSearch/components/SpSmartSearch.module.scss.ts
@@ -0,0 +1,26 @@
+declare const styles: {
+  readonly spSmartSearch: string;
+  readonly searchBar: string;
+  readonly searchInput: string;
+  readonly checkboxGroup: string;
+  readonly searchButton: string;
+  readonly tabContainer: string;
+  readonly tab: string;
+  readonly activeTab: string;
+  readonly resultsContainer: string;
+  readonly loadingSpinner: string;
+  readonly noResults: string;
+  readonly noResultsIcon: string;
+  readonly emptyState: string;
+  readonly resultsList: string;
+  readonly resultItem: string;
+  readonly resultIcon: string;
+  readonly resultContent: string;
+  readonly resultTitle: string;
+  readonly resultMeta: string;
+  readonly resultSummary: string;
+  readonly errorMessage: string;
+  readonly configRequired: string;
+};
+
+export default styles;
