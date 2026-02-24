f5b6af714faea8316dc9a457af659ae9689b0057
faroutroy <68562263+faroutroy@users.noreply.github.com>
2026-02-24 11:31:22 +0000

    Update ISpSmartSearchProps.ts

--- src/webparts/spSmartSearch/components/ISpSmartSearchProps.ts
+++ src/webparts/spSmartSearch/components/ISpSmartSearchProps.ts
@@ -1,11 +1,11 @@
-import { WebPartContext } from '@microsoft/sp-webpart-base';
 import { DisplayMode } from '../models/ISpSmartItem';
+import { WebPartContext } from '@microsoft/sp-webpart-base';
 
 export interface ISpSmartSearchProps {
   context: WebPartContext;
   displayMode: DisplayMode;
   searchScope: 'site' | 'url';
   scopeUrl: string;
-  titleField: string;
-  displayColumn: string;        // ← this is the only setting now
+  titleColumn: string;
+  displayColumns: string;
 }
