23cb79d131e5cbfd9c80db2be3dfcf672cbbad25
faroutroy <68562263+faroutroy@users.noreply.github.com>
2026-02-24 11:11:25 +0000

    Update ISpSmartSearchProps.ts

--- src/webparts/spSmartSearch/components/ISpSmartSearchProps.ts
+++ src/webparts/spSmartSearch/components/ISpSmartSearchProps.ts
@@ -1,33 +1,10 @@
-import { DisplayMode } from '../models/ISpSmartItem';
 import { WebPartContext } from '@microsoft/sp-webpart-base';
-
-export interface IColumnConfig {
-  showTitle: boolean;
-  showAuthor: boolean;
-  showModifiedDate: boolean;
-  showFileType: boolean;
-  showListName: boolean;
-  showBusinessArea: boolean;
-  showBidDate: boolean;
-  showEstimator: boolean;
-  showBid2WinId: boolean;
-  showSegment: boolean;
-  showSqYards: boolean;
-  showLaneMiles: boolean;
-  showNumberOfLots: boolean;
-  showCity: boolean;
-  showCounty: boolean;
-  showState: boolean;
-  showZipCode: boolean;
-  showOwner: boolean;
-  showProject: boolean;
-}
+import { DisplayMode } from '../models/ISpSmartItem';
 
 export interface ISpSmartSearchProps {
   context: WebPartContext;
-  displayMode: DisplayMode;
   searchScope: 'site' | 'url';
   scopeUrl: string;
   titleField: string;
-  columnConfig: IColumnConfig;
+  displayColumn: string;        
 }
